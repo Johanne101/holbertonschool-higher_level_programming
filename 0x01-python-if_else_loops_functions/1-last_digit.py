743abfc1359c4aca1ab75123699e85c604fac749
johanne101 <jlopez0493@gmail.com>
2021-09-08 22:51:21 +0000

    [Test] edited abs flow statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+last_digit = abs(number) % module
 if number < 0:
     last_digit = ((number) % 10) * -1
 else:
     last_digit = number % 10
+
 if last_digit > 5:
     print('Last digit of {:d} is {:d} and is greater than 5'.format(number, last_digit))
 elif last_digit == 0:
