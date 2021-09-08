b0d26d9451d3903e1cc939eb2a9ce9049631daad
johanne101 <jlopez0493@gmail.com>
2021-09-08 22:54:31 +0000

    [EDIT] abs value in flow statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % module
+last_digit = abs(number) % 10
 if number < 0:
-    last_digit = ((number) % 10) * -1
+    last_digit = last_digit * -1
 else:
     last_digit = number % 10
 
