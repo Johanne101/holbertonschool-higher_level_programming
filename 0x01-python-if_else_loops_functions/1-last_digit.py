44a8242e64174a0c9d5f42a1924eda96bd307399
johanne101 <jlopez0493@gmail.com>
2021-09-08 23:11:28 +0000

    [Modified] indentation in script

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,6 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number < 0:
     last_digit = last_digit * -1
-else:
-    last_digit = number % 10
 
 if last_digit > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
