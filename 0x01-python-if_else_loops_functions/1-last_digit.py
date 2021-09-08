50def129542084e54572b4e316842b5d83073384
johanne101 <jlopez0493@gmail.com>
2021-09-08 22:39:40 +0000

    [FIX] control flow statement of digit abs value file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
 if number < 0:
-    last_digit = last_digit * -1
+    last_digit = ((last_digit) % 10) * -1
 
 if last_digit > 5:
     print('Last digit of{:d} is {:d} and is greater than 5'.format(number, last_digit))
