fc35617146e826bb54a821af4cecd8d2558ed32a
johanne101 <jlopez0493@gmail.com>
2021-09-09 00:38:44 +0000

    [HOTFIX] seperation requirements for combo print script

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(x + 1, 10):
-        if x < 9:
-            print("{:d}{:d}".format(x, y), end="")
-        if  x + y != 89:
-            print(", ", end="")
-        else:
-            print("")
+        if x < 8:
+            print("{:d}{:d}, ".format(x, y), end="")
+print("89")
