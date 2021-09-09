b121ec1e9b0ce7d5094a5ba9eba1e128dfc370cc
johanne101 <jlopez0493@gmail.com>
2021-09-09 00:25:48 +0000

    [Mod] file combo script

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(x + 1, 10):
-        print("{:d}{:d}".format(x, y), end="")
-        if  x != 89:
+        if x < 9:
+            print("{:d}{:d}".format(x, y), end="")
+        if  x + y != 89:
             print(", ", end="")
+        else:
+            print("")
