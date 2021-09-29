89d185d5e9130e020743db7adf4c7d9983491f36
johanne101 <jlopez0493@gmail.com>
2021-09-29 15:23:07 +0000

    [Add] file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        while i < x:
+            print(my_list[i], end="")
+            i += 1
+        print()
+        return (i)
+    except Exception:
+        print()
+        return (i)
