e7d4a7ab4e4a9612e9ba4eed6a0f2e3d1219ca90
johanne101 <jlopez0493@gmail.com>
2021-09-29 15:44:42 +0000

    [edit] error type

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     try:
         i = 0
@@ -8,6 +9,7 @@ def safe_print_list(my_list=[], x=0):
             i += 1
         print()
         return (i)
-    except Exception:
+    except IndexError:
         print()
         return (i)
+
