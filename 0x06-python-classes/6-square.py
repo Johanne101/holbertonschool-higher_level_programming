ce802e5c34924d109e7d98f9d9678285f9b1a96d
johanne101 <jlopez0493@gmail.com>
2021-09-30 03:00:43 +0000

    [fix] sintax error in line 38

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,7 +35,7 @@ class Square:
             raise TypeError(text)
         elif len(value) != 2:
             raise TypeError(text)
-        if value[0] >= 0 and value[1] >=0:
+        if value[0] >= 0 and value[1] >= 0:
             self.__position = value
         else:
             self.__position = value
