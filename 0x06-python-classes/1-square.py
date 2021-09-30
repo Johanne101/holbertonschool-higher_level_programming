575c6efd42d649b11bb3cfe046450e02a0958afc
johanne101 <jlopez0493@gmail.com>
2021-09-30 00:46:08 +0000

    [Add] file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""1. Create a class called 'Square'"""
+
+
+class Square:
+    """2. Ceating a defined Square"""
+    __size
+
+    def __init(self, size):
+        """3. Initialize private instance attribute: size."""
+        self.__size = size
