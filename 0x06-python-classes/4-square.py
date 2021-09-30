20822c57e372374fa3b1b3a444149717d460b57b
johanne101 <jlopez0493@gmail.com>
2021-09-30 01:52:39 +0000

    [Add] property & setter function

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""1. Create a class called 'Square' (module)"""
+
+
+class Square:
+    """2. Ceating a defined Square class"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter function of size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ area of square"""
+        return self.__size**2
