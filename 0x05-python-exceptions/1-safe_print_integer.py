6b5e82dac729e3071e1ec08b3d4b4aa06ddf149c
johanne101 <jlopez0493@gmail.com>
2021-09-30 03:06:41 +0000

    [Modified] syntax and error handling

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 
 def safe_print_integer(value):
     try:
-        str.format("{:d}", value)
+        print("{:d}".format(value))
         return (True)
-    except ValueError:
+    except:
         return (False)
