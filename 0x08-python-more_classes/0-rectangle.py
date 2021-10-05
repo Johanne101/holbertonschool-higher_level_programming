7cd950755481ec04dcddd25ae801df5253e3c9b6
johanne101 <jlopez0493@gmail.com>
2021-10-05 00:05:40 +0000

    [Add] empty class files for testing

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Here a class Rectangle is being created. """
+
+
+class Rectangle:
+    """ The command pass after the definition
+    of the class object is created empty."""
+    pass
