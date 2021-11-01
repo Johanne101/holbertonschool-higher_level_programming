81ac57e8e1658952110f5ff558acd4908be2ba36
johanne101 <jlopez0493@gmail.com>
2021-11-01 21:24:23 +0000

    [add] to test code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 
 
 def no_c(my_string):
-   no_c = my_string
-   new_string = no_c.translate({ord(ctr): None for ctr in 'cC'})
-   return(new_string)
+    no_c = my_string
+    new_string = no_c.translate({ord(ctr): None for ctr in 'cC'})
+    return(new_string)
