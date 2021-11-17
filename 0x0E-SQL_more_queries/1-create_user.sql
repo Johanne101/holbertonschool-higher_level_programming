feff1b5b4905712f386a965ae9010225d626389f
johanne101 <jlopez0493@gmail.com>
2021-11-17 22:00:30 +0000

    [Add] root user privileges script

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that creates a user
+-- user_0d_1 has all privileges with setted password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
