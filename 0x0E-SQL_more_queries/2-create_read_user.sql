654a45bf6c84bd25c21e2c39dd7584d4df721f30
johanne101 <jlopez0493@gmail.com>
2021-11-18 15:47:27 +0000

    [add] read user privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- script creates a database and the user
+-- User should have only SELECT privilage
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
