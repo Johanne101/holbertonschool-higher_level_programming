047fe5e754fd1273906500a953ebd80e660edd26
johanne101 <jlopez0493@gmail.com>
2021-11-19 21:00:38 +0000

    [add][edit] states table & Readme

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- script creates hbtn_0d_usa DB and the table states(in the database hbtn_0d_usa)
+-- states description:
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can’t be null
+-- If the database hbtn_0d_usa already exists, script should not fail
+-- If the table states already exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	`id` INT AUTO_INCREMENT,
+	`name` VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+	);
