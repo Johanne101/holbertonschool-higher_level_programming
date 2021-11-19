3c49367bc4ec438f7321268dbecb31597bd0faaa
johanne101 <jlopez0493@gmail.com>
2021-11-19 18:18:22 +0000

    [add] average result script

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- computes the score average of all records in the table
+-- from second_table of hbtn_0c_0 db
+-- The result column name should be average
+SELECT AVG(score) 'average' FROM second_table;
