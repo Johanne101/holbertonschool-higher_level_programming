b06575a44a68435b9c1ef0f825a554000c2f9b77
johanne101 <jlopez0493@gmail.com>
2021-11-20 20:40:35 +0000

    [edit] subquery of city

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,4 @@
 -- script lists all the cities of California that can be found in the database hbtn_0d_usa
--- /1/ SELECT  /2/FROM INFORMATION_SCHEMA.TABLES /3/WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='YOUR_Database_name'
---
--- The states table contains only one record where name = California (but the id can be different, as per the example)
--- /2/ SELECT states(table) FROM 
+-- states table contains only one record where name = California
 -- Results must be sorted in ascending order by cities.id 
--- /3/ ORDER BY cities.id ASC;
-SELECT id, name FROM cities WHERE state_id IN ('states') ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
