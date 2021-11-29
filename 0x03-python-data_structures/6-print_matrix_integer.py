772832fb33b292b7eae71bf0c71218aaa6ed4496
johanne101 <jlopez0493@gmail.com>
2021-11-29 20:31:27 +0000

    [fix] pep8 compliance

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for item in matrix:
         for num in item:
-           print("{:d}".format(num), end='')
-           if num != item[-1]:
-               print(" ", end='')
+            print("{:d}".format(num), end='')
+            if num != item[-1]:
+                print(" ", end='')
         print()
