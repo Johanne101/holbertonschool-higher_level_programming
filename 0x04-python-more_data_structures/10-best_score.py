3a715815c305da909359760b4bd9ac2c330431e5
johanne101 <jlopez0493@gmail.com>
2021-12-02 19:44:14 +0000

    [update] condition

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+"""
+Method: Returns a key with the biggest integer value (big_k).
+"""
+def best_score(a_dictionary):
+    """
+    Assum:
+    - all values are only integers.
+    - all students have a different score.
+    """
+    '''
+    if a_dictionary:
+        dlist = list(a_dictionary)
+        big_k = dlist[0]
+        for key_item in dlist:
+    '''
+    '''
+    if no score found:
+        return None
+    '''
+    '''
+    a_dictionary.sorty()
+    return a_dictionary[-1]
+        else:
+            return None
+    '''
+    if a_dictionary:
+        dscore = max(a_dictionary, key=a_dictionary.get)
+        return dscore
+    else:
+        return None
