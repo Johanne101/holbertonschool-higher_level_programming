e8c5af87b08b19916ad52d5c5492a3bec242a594
johanne101 <jlopez0493@gmail.com>
2021-12-14 09:28:07 +0000

    [fix error]

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-'''
-Module: add_arguments function
-
-script adds all arguments to a Python list, and then save them to a file
-'''
+'''Module: add_arguments function'''
 
 
 import json
 from sys import argv
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
+'''script adds all arguments to a Python list, and then save them to a file'''
 dlist = []
 
 try:
@@ -20,4 +17,4 @@ except:
 for i in range(1, len(argv)):
     dlist.append(argv[i])
 
-save_to_json_file(lista, "add_item.json")
+save_to_json_file(dlist, "add_item.json")
