e470577831c3d327e556a6fb9095823195785fec
johanne101 <jlopez0493@gmail.com>
2022-02-01 04:24:34 +0000

    [Fix] Semistandard Requirements

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const elem of['C is fun', 'Python is cool', 'Javascript is amazing']) {
-	console.log(elem);
+for (const elem of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(elem);
 }
