65e224adcb771f58f04604c46094a9ca275f01ad
johanne101 <jlopez0493@gmail.com>
2022-02-15 01:04:34 +0000

    [add] print number of occurences .js script

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+	console.log('Missing number of occurrences');
+} else {
+	for (; x > 0; x--) {
+		console.log('C is fun');
+	}
+}
