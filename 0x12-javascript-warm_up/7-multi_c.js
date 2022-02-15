00b78ed7a815955bad7963eb6346d41246a674de
johanne101 <jlopez0493@gmail.com>
2022-02-15 01:34:28 +0000

    [fix] semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-
+// script prints x times “C is fun”
 
 let x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (; x > 0; x--) {
-		console.log('C is fun');
-	}
+  for (; x > 0; x--) {
+    console.log('C is fun');
+  }
 }
