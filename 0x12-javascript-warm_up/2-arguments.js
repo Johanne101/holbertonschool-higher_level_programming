14042697b293c489cdbf2ffc9c7e729b15ded187
johanne101 <jlopez0493@gmail.com>
2022-04-05 00:34:44 +0000

    [Fix Code] Semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -10,5 +10,4 @@ if (process.argv.length === 3) {
   console.log('Argument found');
 } else if (process.argv.length < 3) {
   console.log('No argument');
-} else console.log('Arguments found'); {
-}
+} else console.log('Arguments found');
