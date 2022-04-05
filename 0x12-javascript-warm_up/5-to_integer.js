48653241f83c8c27db75913a532afa9ab1a44775
johanne101 <jlopez0493@gmail.com>
2022-04-05 00:46:58 +0000

    [add] function returns int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
+/*
+ * Script prints a msg:
+ * If arg can be converted,
+ * else if can't e converted;
+ * "Not a number" msg will print.
+ */
 
-const numArg = Number(process.argv[2]);
-if (isNaN(numArg)) {
+const arg = Number(process.argv[2]);
+if (isNaN(arg)) {
 	console.log('Not a number');
 } else {
-	console.log('My number: ' + Math.trunc(numArg));
+	console.log('My number: ' + Math.trunc(arg));
 }
