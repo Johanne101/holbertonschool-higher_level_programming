dd5d5444a8343c89dbd9bb392ead1161a377be79
johanne101 <jlopez0493@gmail.com>
2022-04-05 00:55:24 +0000

    [add] Loop script

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
+/*
+ * Script prints 3 lines;
+ * "C is fun"
+ * "Python is cool"
+ * "JavaScript is amazing"
+ */
 
-for (const elem of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(elem);
+for (const str of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(str);
 }
