87b05813d2996deb61313eaeb5759afe2ec33c99
johanne101 <jlopez0493@gmail.com>
2022-04-05 00:28:33 +0000

    [Add] process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
+
 /*
  * Script returns process.argv msg:
  * If no args: “No argument”
  * Else If only 1 arg: “Argument found”
  * Otherwise: “Arguments found”
  */
-if (process.argv.length < 3) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
+if (process.argv.length === 3) {
   console.log('Argument found');
-} else {
-  console.log('Arguments found');
+} else if (process.argv.length < 3) {
+  console.log('No argument');
+} else console.log('Arguments found'); {
 }
