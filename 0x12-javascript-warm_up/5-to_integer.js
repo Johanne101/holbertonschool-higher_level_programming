0514445823ab690e8d144d0349e52c35f18745d9
johanne101 <jlopez0493@gmail.com>
2022-04-05 00:52:11 +0000

    [edit] semistardard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -8,7 +8,7 @@
 
 const arg = Number(process.argv[2]);
 if (isNaN(arg)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + Math.trunc(arg));
+  console.log('My number: ' + Math.trunc(arg));
 }
