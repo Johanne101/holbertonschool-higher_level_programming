3c30c03a95efdb53e16cdb7d2dde092080d85afd
johanne101 <jlopez0493@gmail.com>
2022-04-05 00:20:19 +0000

    [add] 3 languages script

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 /*
- * script that prints 3 lines:
+ * Script prints 3 lines:
  * 1: "C is fun"
  * 2: "Python is cool"
  * 3: "JavaScript is amazin"
  */
+
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
