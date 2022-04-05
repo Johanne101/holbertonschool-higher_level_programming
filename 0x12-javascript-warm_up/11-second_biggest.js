20c7cf85bcb1480f45391bd283548a54c5dcbe5b
johanne101 <jlopez0493@gmail.com>
2022-04-05 01:34:32 +0000

    [add] second Biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
+/*
+ * script searches second biggest int
+ * in list of args;
+ * If no arg "0"
+ * If 1 arg "0"
+ * Else < 2 "output"
+ */
+
 const data = process.argv.slice(2).map(Number);
 const result = data.sort((a, b) => b - a);
 if (process.argv.length < 4) {
