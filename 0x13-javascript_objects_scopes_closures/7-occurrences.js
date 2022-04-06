040e5d58461b0729928aebb6bc540b09711b8245
johanne101 <jlopez0493@gmail.com>
2022-04-06 01:57:01 +0000

    [add] Ocurrences Script

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Function to return number of occurences in a list
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (let i = 0; i <= list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
