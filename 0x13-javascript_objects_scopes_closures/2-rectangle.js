1b6286e143aae36d6241b3d1784679c76d8d992d
johanne101 <jlopez0493@gmail.com>
2022-04-06 00:38:31 +0000

    [add] Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Class Rectangle defines;
+// w = width
+// h = height
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
